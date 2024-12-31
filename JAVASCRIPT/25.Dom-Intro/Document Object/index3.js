42acda1857d5b712d77f61fb1a805b6613be27f6
AMAN SINGH <amansinghbhumihar2510@gmail.com>
2024-12-31 19:20:14 +0530

    Added

--- JAVASCRIPT/25.Dom-Intro/Document Object/index3.js
+++ JAVASCRIPT/25.Dom-Intro/Document Object/index3.js
@@ -1,3 +1,3 @@
 console.log("Written js from index3.js");
 console.log("end");
-document.title = "Aman Singh";//to change the title of html from JS
+document.title = "Aman Singh";//to change the title of html from JS and using Dom We can do many other stuff.
